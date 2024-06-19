5a7c408260a4cf81b260cc8fa52103857ee73b47
swapnil7927 <sahuswapnil2000@gmail.com>
2024-06-20 01:53:56 +0530

    Deployment

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-import connectToMongoDB from "./db/connecttoMongoDB.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from './socket/socket.js'
 const PORT = process.env.PORT || 5000;
  
